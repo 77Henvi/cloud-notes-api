41ceef9218ac06f89ac05e91ae08d0c8323131c1
77Henvi <nitakornr@gmail.com>
2026-04-13 20:14:25 +0700

    add cors

---
+++ frontend/src/App.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await fetch("https://cloud-notes-api-qeyj.onrender.com/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setMessage("Login success 🔥");
+        console.log("TOKEN:", data.token);
+
+        // เก็บ token
+        localStorage.setItem("token", data.token);
+      } else {
+        setMessage(data.message);
+      }
+    } catch (err) {
+      setMessage("Error connecting to server");
+    }
+  };
+
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={handleLogin}>Login</button>
+
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
